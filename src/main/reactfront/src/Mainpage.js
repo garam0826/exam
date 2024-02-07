a19ed558505d47fe8c5da7b2d1c305522c02b57b
hjpark0417 <duffy35@naver.com>
2024-02-07 16:37:24 +0900

    로그인 버튼 추가

--- src/main/reactfront/src/Mainpage.js
+++ src/main/reactfront/src/Mainpage.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { Link } from "react-router-dom"
+import {useNavigate} from "react-router-dom";
 
 import './App.css';
 
 const Mainpage = () => {
+    const navigate = useNavigate();
     return (
-        /*<h1>Main page</h1>
+        //<h1>Main page</h1>
         <div>
+            [ Login ]
+            <button onClick={() => navigate("/login")}>로그인</button>
+            <hr/>
+            [ SignUp ]
+            <button onClick={() => navigate("/signup")}>회원가입</button>
+        </div>
+
+
+        /*<div>
             <Link to={'/login'}>Login</Link>
-        </div>*/
+        </div>*!/
         <div>
             <Link to={'/signup'}>SignUp</Link>
-        </div>
+        </div>*/
     );
 }
 
