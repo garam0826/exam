234a88f65f2b7eb65bb38ec37c76e54eb2aff446
garam0826 <garam0826@naver.com>
2024-02-12 20:12:11 +0900

    controller update

--- src/main/java/com/example/test/controller/ApiMemberController.java
+++ src/main/java/com/example/test/controller/ApiMemberController.java
@@ -5,19 +5,16 @@ import com.example.test.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/member")
+@RequestMapping("/member")
 public class ApiMemberController {
 
     @Autowired
     private MemberService memberService; // 회원 정보를 조회하는 로직을 처리
 
-    // 로그인 처리 API
+    // 로그인 처리
     @PostMapping("/login")
     public ResponseEntity<MemberDTO> login(@RequestBody MemberDTO memberDTO) {
         try {
@@ -35,7 +32,7 @@ public class ApiMemberController {
         }
     }
 
-    // 회원가입 API
+    // 회원가입
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody MemberDTO memberDTO) {
         try {
@@ -50,6 +47,64 @@ public class ApiMemberController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    //회원탈퇴
+    @PostMapping("/mypage/resign")
+    public ResponseEntity<String> deleteMember(@RequestBody MemberDTO memberDTO) {
+        try {
+            int result = memberService.deleteMember(memberDTO.getMem_id());
+            if (result>0) {
+                return new ResponseEntity<>("회원탈퇴 성공", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("회원탈퇴 실패", HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    //회원정보 조회(/member/mypage//myinfo)
+    @GetMapping("/{mem_id}")
+    public ResponseEntity<MemberDTO> getMemberInfo(@PathVariable String mem_id) {
+        try {
+            MemberDTO memberInfo = memberService.selectMember(mem_id);
+            if (memberInfo != null) {
+                return new ResponseEntity<>(memberInfo, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    //회원정보 수정
+    @PutMapping("/mypage/myedit")
+    public ResponseEntity<String> updateMember(@RequestBody MemberDTO memberDTO) {
+        try {
+            int result = memberService.updateMember(memberDTO);
+            if (result > 0) {
+                return new ResponseEntity<>("회원정보 수정 성공", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("회원정보 수정 실패", HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    //아이디 찾기(/searchid)
+
+    //비밀번호 찾기(/searchpw)
+
+    //찜(/mycart)
+
+
+
+
 
 }
 
